5089b1dd6188ce62aafd259590ba1f54e88f977b
Junho Park <jhpark870708@daum.net>
2019-10-18 10:35:13 +0900

    Java Config settings without web.xml

---
+++ src/main/java/kr/co/itcen/hellospring/config/WebConfig.java
@@ -0,0 +1,25 @@
+package kr.co.itcen.hellospring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan({ "kr.co.itcen.hellospring.controller" })
+public class WebConfig {
+
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		//viewResolver.setViewClass(JstlView.class);
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		viewResolver.setExposeContextBeansAsAttributes(true);
+		return viewResolver;
+	}
+
+}
